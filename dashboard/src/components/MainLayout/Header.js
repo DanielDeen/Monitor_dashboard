d41317bb92f1bc0af95dbb61aa1c1f7ae65e444f
DanielDeen <ding.xuyong@gmail.com>
2017-07-30 16:53:14 +0800

    add_loading

--- dashboard/src/components/MainLayout/Header.js
+++ dashboard/src/components/MainLayout/Header.js
@@ -3,16 +3,16 @@ import { Menu, Icon } from 'antd';
 import { Link } from 'dva/router';
 
 function Header ({ location }) {
-	return (
-		<Menu
-			selectedKeys={[location.pathname]}
-			mode="horizontal"
-			theme="dark"
-		>
-		<Menu.Item key="/users">
-			<Link to="/users"><Icon type="bars" />Users</Link>
-		</Menu.Item>
-		<Menu.Item key="/">
+  return (
+    <Menu
+      selectedKeys={[location.pathname]}
+      mode="horizontal"
+      theme="dark"
+    >
+    <Menu.Item key="/users">
+      <Link to="/users"><Icon type="bars" />Users</Link>
+    </Menu.Item>
+    <Menu.Item key="/">
 			<Link to="/"><Icon type="home" />Home</Link>
 		</Menu.Item>
 		<Menu.Item key="/404">
--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,5 +1,6 @@
 import dva from 'dva';
 import './index.css';
+import createLoading from 'dva-loading';
 
 // 1. Initialize
 const app = dva();
@@ -7,6 +8,7 @@ const app = dva();
 
 // 2. Plugins
 // app.use({});
+app.use(createLoading());
 
 // 3. Model
 // app.model(require('./models/example'));
